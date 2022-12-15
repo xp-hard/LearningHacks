bb2c24b50772a5ecc7389c2365ba91931d5e607f
xp-hard <mhlsjk@gmail.com>
2022-12-15 22:33:20 +0300

    dll to check if injector works fine

---
+++ dllmain.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <thread>
+
+void HackThread(HMODULE instance) {
+	AllocConsole();
+	FILE* file;
+	freopen_s(&file, "CONOUT$", "w", stdout);
+
+	while (!GetAsyncKeyState(VK_END)) {
+		std::cout << "Injected\n";
+	}
+
+	if (file) {
+		fclose(file);
+	}
+
+	FreeConsole();
+	FreeLibraryAndExitThread(instance, 0);
+}
+
+BOOL WINAPI DllMain(HMODULE instace, DWORD reason, LPVOID lpvReserved) {
+	if (reason == DLL_PROCESS_ATTACH) {
+		DisableThreadLibraryCalls(instace);
+
+		std::thread(HackThread, instace).detach();
+	}
+}
